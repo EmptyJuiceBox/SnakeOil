b1409c93ec7e2ea072aca9fb87a952c400fd06cf
mortie <martid0311@gmail.com>
2016-09-25 22:59:05 +0200

    don't mess with my whitespace :(

--- server/js/room.js
+++ server/js/room.js
@@ -44,8 +44,8 @@ module.exports = class Room {
 	}
 
 	// Remove a player.
-	//    Destroy the room if the player is the operator.
-	//    Emit a /players event to all players.
+	//     Destroy the room if the player is the operator.
+	//     Emit a /players event to all players.
 	removePlayer(player) {
 		if (player === this.operator) {
 			this.destroy();
@@ -135,8 +135,8 @@ module.exports = class Room {
 	 */
 
 	// End a pitch.
-	//	   If there's no more players who can be pitchers,
-	//	   the pitcher will be undefined (and thus null in /roles)
+	//     If there's no more players who can be pitchers,
+	//     the pitcher will be undefined (and thus null in /roles)
 	roundPitchEnd() {
 		clearTimeout(this.pitchTimeout);
 
@@ -207,7 +207,7 @@ module.exports = class Room {
 			this.customer = null;
 			this.pitcher = null;
 			this.emit("/roles");
-            this.emit("/players");
+			this.emit("/players");
 			return;
 		}
 
